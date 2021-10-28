06c3db540d728041c3b3d6793706d756036f4e2d
Andyyoshen <wwee123096@gmail.com>
2021-10-28 20:54:30 +0800

    '修改驗證碼樣式'

--- Model/General/ImageCode.js
+++ Model/General/ImageCode.js
@@ -6,10 +6,12 @@ module.exports.getImageCode = async function(){
         width:160,
         height:60,
         fontSize: 50,
-        ignoreChars: '0oO1ilI', // 验证码字符中排除 0o1i
-        noise: 5, // 干扰线条的数量
+        mathMin:1,
+        mathMax:9,
+        ignoreChars: '0oO1ilIabcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ', // 验证码字符中排除 0o1i
+        noise: 2, // 干扰线条的数量
         color: true, // 验证码的字符是否有颜色，默认没有，如果设定了背景，则默认有
-        background: '#eee' // 验证码图片背景颜色
+        background: '#7B7B7B' // 验证码图片背景颜色
     })
     return cap
     
